08a5ae1878f7b59e8cc804078676a5232b7dd5a1
nlafrance <nlafrance@coveo.com>
2019-10-18 16:42:06 -0400

    Started adding some functinality to ViewedFilter SFINT-2518

--- src/components/ViewedFilter/ViewedFilter.ts
+++ src/components/ViewedFilter/ViewedFilter.ts
@@ -7,22 +7,24 @@ import {
     Checkbox,
     load,
     l,
-    IFacetOptions,
+    IQueryResult,
+    ResultList
 } from 'coveo-search-ui';
+import { find } from 'underscore';
 import { ViewedFilterEvents, IViewedFilterEventArgs } from './Events';
 import './Strings';
 
 /**
  * Metadata sent when an analytics event is sent.
  */
-export interface IAnalyticsFilteredResultsMeta {
+export interface IAnalyticsHiddenResultsMeta {
     hiddenResults: boolean;
 }
 
 /**
  * Possible options to configure the **ViewedFilter** component.
  */
-export interface IViewedFilterOptions extends IFacetOptions {
+export interface IViewedFilterOptions extends ComponentOptions {
 
 }
 
@@ -33,10 +35,17 @@ export interface IViewedFilterOptions extends IFacetOptions {
 export class ViewedFilter extends Component {
     static ID = 'ViewedFilter';
     private checkbox: Checkbox;
+    private resultList: ResultList;
 
     static options: IViewedFilterOptions = {
     };
 
+    /**
+     * Create an instance of ViewedFilter
+     * @param element 
+     * @param options 
+     * @param bindings 
+     */
     constructor(public element: HTMLElement, public options: IViewedFilterOptions, public bindings?: IComponentBindings) {
         super(element, ViewedFilter.ID, bindings);
 
@@ -58,14 +67,17 @@ export class ViewedFilter extends Component {
     }
 
     protected initialize(): void {
+        this.initPresentational();
+        this.findResultList();
+    }
+
+    private initPresentational() {
         const headerSection = $$('div', { className: 'coveo-facet-header' });
-        const headerTitleDiv = $$('div', {
-            className: 'coveo-facet-header-title',
-        }).el;
+        const headerTitleDiv = $$('div', { className: 'coveo-facet-header-title',}).el;
         headerTitleDiv.innerHTML = l('ViewedFilterHeader_Label')
         headerSection.append(headerTitleDiv);
 
-        const valueSection = $$('div', { className: 'coveo-facet-value' } );
+        const valueSection = $$('div', { className: 'coveo-facet-value' });
         const labelDiv = $$('label', {
             className: 'coveo-facet-value-label-wrapper'
         }).el;
@@ -81,14 +93,42 @@ export class ViewedFilter extends Component {
     }
 
     private async createCheckbox(): Promise<Checkbox> {
-        if (Coveo.Checkbox === undefined) {
+        if (Checkbox === undefined) {
             await load('Checkbox');
         }
         return new Checkbox(this.handleCheckboxChange.bind(this), l('ViewedByCustomerFilter_Label'));
     }
 
-    private handleCheckboxChange(checkbox: Checkbox) {
-        Coveo.$$(this.root).trigger(ViewedFilterEvents.Click, { checked: this.checkbox.isSelected() } as IViewedFilterEventArgs);
+    private findResultList() {
+        const resultLists = $$(this.root).findAll(`.${Component.computeCssClassName(ResultList)}`).map(el => <ResultList>Component.get(el, ResultList));
+        this.resultList = find(resultLists, resultList => !resultList.disabled);
+    }
+
+    private async handleCheckboxChange(checkbox: Checkbox) {
+        $$(this.root).trigger(ViewedFilterEvents.Click, { checked: this.checkbox.isSelected() } as IViewedFilterEventArgs);
+        this.findResultList();
+
+        if (this.resultList) {
+            await this.filterResults(this.resultList).then( async filteredResults => {
+                if (filteredResults.length > 0) {
+                    await this.resultList.renderResults(filteredResults, false);
+                }
+            });
+        }
+    }
+
+    private async filterResults(resultList: ResultList) {
+        const resultElements: HTMLElement[] = [];
+        const results: IQueryResult[] = resultList.getDisplayedResults();
+        console.log(this.resultList.getDisplayedResults());
+        await Promise.all(results.map( async result => {
+            if (!result.isUserActionView) {
+                await resultList.buildResult(result).then( builtResult => {
+                    resultElements.push(builtResult);
+                });
+            }
+        }));
+        return resultElements;
     }
 }
 
